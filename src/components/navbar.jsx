82ba5de77b55b60023f61813218a3e9361c7c441
vishal k <vishalcodex07@gmail.com>
2025-12-11 19:44:56 +0530

    added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,16 +13,16 @@ export default function Navbar() {
         fixed top-0 left-0 w-full 
         flex justify-between items-center
         p-4 sm:p-6 md:p-8
-        text-white z-50
+        text-white z-50 mb-40
       "
     >
 
       {/* Left Title */}
       <div className="goldman-bold 
-        text-lg sm:text-xl md:text-2xl lg:text-3xl 
+        text-lg sm:text-xl md:text-xl lg:text-2xl 
         tracking-wide
       ">
-        Hey, I'm Vishal
+     Vishal
       </div>
 
       {/* Right Menu */}
@@ -31,7 +31,7 @@ export default function Navbar() {
           flex 
           gap-4 sm:gap-6 md:gap-8 lg:gap-12
           text-sm sm:text-lg md:text-xl lg:text-2xl
-          goldman-regular hover: text-gray-400
+          goldman-regular  text-gray-400 
         "
       >
         <button onClick={() => scrolltosection("home")}>HOME</button>
